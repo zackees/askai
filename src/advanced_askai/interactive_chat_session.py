148046d726db0e1815d2e24d1f9568ffaf5ba285
zackees <z@zackees.com>
2024-07-22 16:29:43 -0700

    Refactor single_chat_session and interactive_chat_session

--- src/advanced_askai/interactive_chat_session.py
+++ src/advanced_askai/interactive_chat_session.py
@@ -7,6 +7,57 @@ from advanced_askai.run_chat_query import run_chat_query
 from advanced_askai.streams import NullOutStream, OutStream
 
 
+def single_chat_session(
+    chatbot: ChatBot,
+    prompt: str,
+    output: Optional[str],
+    as_json: bool,
+    no_stream: bool,
+    check: bool,
+) -> int:
+    """Runs a single chat query, throws exceptions if there are issues"""
+    output_stream: OutStream = OutStream(output)
+    rtn: Optional[int] = None
+    if check:
+        # if we're checking, we need to use a null output stream
+        output_stream.close()
+        output_stream = NullOutStream()
+
+    try:
+        rtn = run_chat_query(
+            chatbot=chatbot,
+            prompts=[prompt],
+            output_stream=output_stream,
+            as_json=as_json,
+            no_stream=no_stream,
+            output=output,
+            print_status=True,
+        )
+        if rtn is not None:
+            return rtn
+
+        if check:
+            output_stream.close()
+            output_stream = OutStream(output)
+            check_prompt = AI_ASSISTANT_CHECKER_PROMPT + "\n\n" + prompt
+            print("\n############ CHECKING RESPONSE")
+            rtn = run_chat_query(
+                chatbot=chatbot,
+                prompts=[check_prompt],
+                output_stream=output_stream,
+                as_json=as_json,
+                no_stream=no_stream,
+                output=output,
+                print_status=False,
+            )
+            output_stream.close()
+            if rtn is not None:
+                return rtn
+    finally:
+        output_stream.close()
+    return 0
+
+
 def interactive_chat_session(
     chatbot: ChatBot,
     prompts: list[str],
@@ -24,11 +75,6 @@ def interactive_chat_session(
             "\nInteractive mode - press return three times to submit your code to OpenAI"
         )
     while True:
-        rtn: Optional[int] = None
-        output_stream: OutStream = OutStream(output)  # needs to be created every time.
-        if check:
-            # if we're checking, we need to use a null output stream
-            output_stream = NullOutStream()
         new_cmd = prompts[-1].strip().replace("()", "")
         if new_cmd.startswith("!"):
             prompts = prompts[0:-1]
@@ -41,36 +87,17 @@ def interactive_chat_session(
             print("Exited due to 'exit' command")
             return 0
 
-        rtn = run_chat_query(
+        rtn = single_chat_session(
             chatbot=chatbot,
-            prompts=prompts,
-            output_stream=output_stream,
+            prompt="\n".join(prompts),
+            output=output,
             as_json=as_json,
             no_stream=no_stream,
-            output=output,
-            print_status=True,
+            check=check,
         )
-        output_stream.close()
-        if rtn is not None:
-            return rtn
 
-        if check:
-            output_stream = OutStream(output)  # needs to be created every time.
-            check_prompt = AI_ASSISTANT_CHECKER_PROMPT + "\n\n" + prompts[-1]
-            prompts.append(check_prompt)
-            print("\n############ CHECKING RESPONSE")
-            rtn = run_chat_query(
-                chatbot=chatbot,
-                prompts=prompts,
-                output_stream=output_stream,
-                as_json=as_json,
-                no_stream=no_stream,
-                output=output,
-                print_status=False,
-            )
-            output_stream.close()
-            if rtn is not None:
-                return rtn
+        if rtn != 0:
+            return rtn
 
         if not interactive:
             return 0
